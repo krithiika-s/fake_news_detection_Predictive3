748ef8331de7a6ba7a353ceae4714ddd0f04cee2
krithiika-s <krithikashaju@gmail.com>
2026-05-14 19:55:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,11 @@ import joblib
 
 model = joblib.load("model/liar_model.pkl")
 
-st.title("📰 LIAR Fake News Detection App")
+st.title("LIAR Fake News Detection")
 
-user_input = st.text_area("Enter a statement")
+text = st.text_area("Enter statement")
 
 if st.button("Predict"):
-    if user_input:
-        pred = model.predict([user_input])[0]
-        st.success(f"Prediction: {pred}")
-    else:
-        st.warning("Please enter text")
+    if text:
+        pred = model.predict([text])[0]
+        st.success(pred)
